a23e4e6e2c6ec47f308c5c1261d58a842d2b8396
藤村健吾 <naruawaryousuke@gmail.com>
2017-09-22 17:45:05 +0900

    bボタンだけうごくやつや

---
+++ b_botton/b_botton.ino
@@ -0,0 +1,23 @@
+
+#define DAT 6
+#define DATA2   7
+#define CLK  8
+
+void setup(){  
+
+  pinMode(DAT,INPUT);
+  pinMode(CLK,OUTPUT);
+  pinMode(PS,OUTPUT);
+   Serial.begin(9600);
+}//20
+
+void loop(){
+
+    digitalWrite(DATA2,HIGH);
+    digitalWrite(CLK,HIGH);
+    digitalWrite(DATA2,LOW);
+    digitalWrite(CLK,LOW);
+    if(digitalRead(DAT) == LOW){
+      Serial.println("press_b_bottom");                        
+   }
+}
